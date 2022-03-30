e4c7f252f346077c27b9dc150fdabd61f1e42757
Sam Minot <sminot@fredhutch.org>
2022-03-30 11:54:42 -0500

    Reference version from relative import

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import bash_workbench
+from . import bash_workbench
 
 # Read the requirements from requirements.txt
 with open("requirements.txt", "rt") as handle:
